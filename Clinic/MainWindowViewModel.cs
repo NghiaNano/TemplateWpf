92700a51e465977798d1dbd493584e1c1afd1b3e
NghiaNano <163148999+NghiaNano@users.noreply.github.com>
2024-03-16 21:07:39 +0700

    Init

---
+++ Clinic/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+
+namespace Clinic
+{
+
+	public class MainWindowViewModel
+	{
+		public IRelayCommand CalculateCommand { get; }
+		public MainWindowViewModel()
+		{
+			CalculateCommand = new RelayCommand(
+				execute: () =>
+					{
+								
+					},
+				canExecute: () =>
+				{
+					return true;
+				});
+		}
+	}
+}
